8656d6ed0cc292895db0b88d54b2c240cfc3e7d1
Turan141 <darkhero140@gmail.com>
2022-02-05 16:19:39 +0400

    second s '

--- src/Modules/UsersList/UsersList.js
+++ src/Modules/UsersList/UsersList.js
@@ -1,41 +1,35 @@
 import React from "react";
 import "./UsersList.css";
+import { Bars } from "react-loader-spinner";
+import Users from "./UserListFn";
 
 class UsersList extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = { data: [], isLoading: true };
   }
 
   async componentDidMount() {
+    var that = this;
     const response = await fetch(`https://jsonplaceholder.typicode.com/users`);
     const json = await response.json();
-    console.log(json);
-    this.setState({ data: json, isLoading: false });
+    setTimeout(function () {
+      that.setState({
+        data: json,
+        isLoading: false,
+      });
+    }, 2000);
   }
 
   render() {
     if (this.state.isLoading) {
-      return <div>Waiting...</div>;
+      return (
+        <div className="Spinner">
+          <Bars color="#00BFFF" height={200} width={200} />
+        </div>
+      );
     }
-
-    return (
-      <div className="UserDiv">
-        <ul className="UserUl">
-          <h1>Список пользователей</h1>
-          {this.state.data.map((el) => (
-            <li className="UserLi" key={el.name}>
-              ФИО: {el.name}
-              <br></br>
-              Город: {el.address.city}
-              <br></br>
-              Компания: {el.company.name}
-              <button className="userBtn">Подробнее</button>
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
+    return <Users props={this.state} />;
   }
 }
 
