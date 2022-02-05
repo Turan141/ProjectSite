731ec84f0960f44dea9be12b0aba21d549b79ddc
Turan141 <darkhero140@gmail.com>
2022-02-05 13:56:31 +0400

    second y

--- src/Modules/UsersList/UsersList.js
+++ src/Modules/UsersList/UsersList.js
@@ -1,23 +1,39 @@
 import React from "react";
 import "./UsersList.css";
 
-let userList = [];
-
 class UsersList extends React.Component {
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((data) => {
-        return data.json();
-      })
-      .then((data) => {
-        return console.log(data);
-      });
+  constructor() {
+    super();
+    this.state = { data: [], isLoading: true };
+  }
+
+  async componentDidMount() {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/users`);
+    const json = await response.json();
+    console.log(json);
+    this.setState({ data: json, isLoading: false });
   }
 
   render() {
+    if (this.state.isLoading) {
+      return <div>Waiting...</div>;
+    }
+
     return (
-      <div className="UsersList">
-        <h1>Список пользователей</h1>
+      <div className="UserDiv">
+        <ul className="UserUl">
+          <h1>Список пользователей</h1>
+          {this.state.data.map((el) => (
+            <li className="UserLi" key={el.name}>
+              ФИО: {el.name}
+              <br></br>
+              Город: {el.address.city}
+              <br></br>
+              Компания: {el.company.name}
+              <button className="userBtn">Подробнее</button>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
