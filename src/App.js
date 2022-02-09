b7cf6866f5301b668a044da93893351eec6a676d
Turan141 <darkhero140@gmail.com>
2022-02-09 18:02:35 +0400

    second1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import MainWindow from "./Modules/MainWindow/MainWindow";
 import "./App.css";
+import UsersList from "./Modules/UsersList/UsersList";
 
 function App() {
-  return <MainWindow />;
+  return (
+    <>
+      <MainWindow />
+    </>
+  );
 }
 export default App;
--- src/Modules/UsersList/UsersList.js
+++ src/Modules/UsersList/UsersList.js
@@ -1,16 +1,30 @@
 import React from "react";
+import NewComponent from "../UserComponent/NewComponent";
 import "./UsersList.css";
 
 class UsersList extends React.Component {
   constructor() {
     super();
-    this.state = { data: [], isLoading: true, userInfo: [] };
+    this.state = {
+      data: [],
+      isLoading: true,
+      userInfo: [],
+      showComponent: false,
+      name: "",
+    };
     this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   handleClick(e) {
-    this.setState({ userInfo: e });
+    console.log(e);
+    let id = e.id;
+    this.setState({ userInfo: id });
     console.log(this.state.userInfo);
+  }
+
+  handleChange(e) {
+    this.setState({ name: e.target.value });
   }
 
   async componentDidMount() {
@@ -26,6 +40,7 @@ class UsersList extends React.Component {
   }
 
   render() {
+    const { name } = this.state;
     if (this.state.isLoading) {
       return (
         <div className="Spinner">
@@ -39,19 +54,33 @@ class UsersList extends React.Component {
         <ul className="UserUl">
           <h1>Список пользователей</h1>
           {this.state.data.map((el) => (
-            <li className="UserLi" key={el.name}>
+            <li className="UserLi" key={el.id}>
               ФИО: {el.name}
               <br></br>
               Город: {el.address.city}
               <br></br>
               Компания: {el.company.name}
-              <button onClick={this.handleClick} className="userBtn">
+              <button
+                onClick={() => {
+                  this.setState({ userInfo: el });
+                  this.setState({ showComponent: true });
+                  console.log(this.state.userInfo);
+                }}
+                className="userBtn"
+              >
                 Подробнее
               </button>
             </li>
           ))}
           <p>Найдено {this.state.data.length} элементов</p>
         </ul>
+        {this.state.showComponent ? (
+          <NewComponent
+            props={this.state.userInfo}
+            handleChange={this.handleChange}
+            name={name}
+          />
+        ) : null}
       </div>
     );
   }
